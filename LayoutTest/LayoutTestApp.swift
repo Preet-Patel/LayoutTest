fb438305b9bf625c58516d631d7848f5d1418fd5
Preet Pambhar <preetpambhar73530@gmail.com>
2024-01-19 22:55:19 -0500

    Initial Commit

---
+++ LayoutTest/LayoutTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutTestApp.swift
+//  LayoutTest
+//
+//  Created by Preet Pambhar on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct LayoutTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
